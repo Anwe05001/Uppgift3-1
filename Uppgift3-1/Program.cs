4b1cc3fd9619e05741b9b11c0a07e91d302cf698
Pingvu <nhlpool27@gmail.com>
2023-10-19 12:50:36 +0200

    Add project files.

---
+++ Uppgift3-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hur gammal är du?");
+        string ålder = Console.ReadLine();
+        int age = int.Parse(ålder);
+        
+
+        if (age > 19)
+        {
+            Console.WriteLine("Du är tyvärr för gammal");
+        }
+
+        if (age < 16)
+        {
+            Console.WriteLine("Du är tyvärr för ung");
+        }
+
+        else if (age !< 16 && age !> 19)
+        {
+            Console.WriteLine("Du får delta");
+        } 
+
+        Console.ReadKey();
+    }
+}
